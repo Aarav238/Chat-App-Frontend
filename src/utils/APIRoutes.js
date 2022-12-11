ca761645303c9467f51e73abf5a03a3462eb2254
Aarav238 <aarav8090shukla@gmail.com>
2022-12-11 18:36:48 +0530

    added backend url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
- export const host = "http://localhost:5000";
-// export const host = "https://chat-app-aarav.herokuapp.com";
+ //export const host = "http://localhost:5000";
+  export const host = "https://buzz-chat-um1v.onrender.com/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
